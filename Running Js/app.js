844f5809a3d2363df5e905bde8549ddc41d29392
Aakash Pawan G P S <aakashpawangps@gmail.com>
2021-08-23 23:52:45 +0530

    Basic To Do

---
+++ Running Js/app.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+let total = 1 + 3;
+let age = prompt("Enter your age");
+if (age > 18) {
+    console.log("Eligible to Vote");
+    alert("Eligible to Vote");
+} else {
+    console.log("Not Eligible");
+    alert("Not Eligible");
+}
